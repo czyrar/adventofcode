4cfff3b5ff91d1bdea84d8039c758a275cf37079
C. Roldán Piñero <roldanpineroc@disroot.org>
2025-12-03 09:58:29 +0100

    2025 day 2

---
+++ 2025/rust/day2/src/main.rs
@@ -0,0 +1,9 @@
+mod parts;
+use crate::parts::part1::*;
+use crate::parts::part2::*;
+
+fn main() {
+    let input = include_str!("../data/input.dat");
+    println!("PART 1: the sum of invalid IDs is {}", part1(input));
+    println!("PART 2: the sum of invalid IDs is {}", part2(input));
+}
---
+++ 2025/rust/day2/src/parts/part1.rs
@@ -0,0 +1,17 @@
+use onig::Regex;
+
+pub fn part1(input: &str) -> i64 {
+    let re = Regex::new(r"^(\d+)\1$").unwrap();
+    let mut sum_invalid: i64 = 0;
+    for part in input.split(",") {
+        let range = part.trim().split("-").collect::<Vec<&str>>();
+        let start = range[0].parse::<i64>().unwrap();
+        let end = range[1].parse::<i64>().unwrap();
+        for i in start..=end {
+            if re.is_match(i.to_string().as_str()) {
+                sum_invalid += i;
+            }
+        }
+    }
+    return sum_invalid;
+}
