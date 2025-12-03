2f441c488987993e0c9d8b09c256eb1d4ebeeaf9
C. Roldán Piñero <roldanpineroc@disroot.org>
2025-12-03 13:47:37 +0100

    OPTIMIZATION 2025 day 3

--- 2025/rust/day3/src/parts/part1.rs
+++ 2025/rust/day3/src/parts/part1.rs
@@ -2,14 +2,14 @@ pub fn part1(input: &str) -> i32 {
     let mut joltvolt: u32 = 0;
     for line in input.lines() {
         let (mut i, mut z) = (0, 0u32);
+        let digits: Vec<u32> = line.chars().map(|x| x.to_digit(10).unwrap()).collect();
         for dig in 0..2 {
-            (i, z) = line
-                .chars()
+            (i, z) = digits
+                .iter()
                 .skip(i)
-                .map(|x| x.to_digit(10).unwrap())
                 .enumerate()
                 .filter(|&(j, _)| j < line.len() - 1 + dig - i)
-                .min_by_key(|(_, x)| 9 - x)
+                .min_by_key(|&(_, x)| 9 - x)
                 .map(|(j, x)| (j + i + 1, (z * 10) + x))
                 .unwrap();
         }
--- 2025/rust/day3/src/parts/part2.rs
+++ 2025/rust/day3/src/parts/part2.rs
@@ -2,14 +2,17 @@ pub fn part2(input: &str) -> i64 {
     let mut joltvolt: u64 = 0;
     for line in input.lines() {
         let (mut i, mut z) = (0, 0u64);
+        let digits: Vec<u64> = line
+            .chars()
+            .map(|x| x.to_digit(10).unwrap() as u64)
+            .collect();
         for dig in 0..12 {
-            (i, z) = line
-                .chars()
+            (i, z) = digits
+                .iter()
                 .skip(i)
-                .map(|x| x.to_digit(10).unwrap() as u64)
                 .enumerate()
                 .filter(|&(j, _)| j < line.len() - 11 + dig - i)
-                .min_by_key(|(_, x)| 9 - x)
+                .min_by_key(|&(_, x)| 9 - x)
                 .map(|(j, x)| (j + i + 1, (z * 10) + x))
                 .unwrap();
         }
